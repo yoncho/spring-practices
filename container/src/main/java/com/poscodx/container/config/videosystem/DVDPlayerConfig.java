d6aa4cb095c4dd2e40be5fd42eb5b9c2f7999124
yoncho <joyonghyeon13@gmail.com>
2023-10-05 09:49:36 +0900

    study - injection (Bean, Quailify, Configuration)

--- container/src/main/java/com/poscodx/container/config/videosystem/DVDPlayerConfig.java
+++ container/src/main/java/com/poscodx/container/config/videosystem/DVDPlayerConfig.java
@@ -1,8 +1,32 @@
 package com.poscodx.container.config.videosystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.poscodx.container.videosystem.Avengers;
+import com.poscodx.container.videosystem.DVDPlayer;
+import com.poscodx.container.videosystem.DigitalVideoDisc;
 
 @Configuration
 public class DVDPlayerConfig {
-	
+	@Bean
+	public DigitalVideoDisc avengers() {
+		return new Avengers();
+	}
+
+	// 주입 (Injection)하기 I
+	// Bean 생성 메소드를 직접 호출하는 방법
+	// 생성자 주입
+	@Bean("dvdPlayer")
+	public DVDPlayer dvdPlayer01() {
+		return new DVDPlayer(avengers());
+	}
+
+	// 주입 (Injection)하기 II
+	// Parameter로 Bean을 전달하는 방법
+	// 생성자 주입 (강사님 추천!! 방식)
+	@Bean
+	public DVDPlayer dvdPlayer02(DigitalVideoDisc dvd) {
+		return new DVDPlayer(avengers());
+	}
 }
--- container/src/main/java/com/poscodx/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/poscodx/container/videosystem/DVDPlayer.java
@@ -3,6 +3,10 @@ package com.poscodx.container.videosystem;
 public class DVDPlayer {
 	private DigitalVideoDisc dvd;
 	
+	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd=dvd;
+	}
+	
 	public String play() {
 		return dvd.play();
 	}
